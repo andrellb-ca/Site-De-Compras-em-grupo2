59d19f24ca195bf35a23c7e0acdd029fa57392a0
andrellb-ca <andre.l.borges14@aluno.senai.br>
2026-09-11 14:44:33 -0300

    Add files via upload

--- loja2.py
+++ loja2.py
@@ -5,9 +5,9 @@ while True:
     print('-' * 50)
     print('                Bem-vindo a loja')
     print('-' * 50)
-    print('1 - Carne')
-    print('2 - Açucar')
-    print('3 - Chocolate')
+    print('1 - Tênis')
+    print('2 - Microfone')
+    print('3 - Mouse')
     print('4 - Fone De Ouvido')
     print('5 - PlayStation 5')
     print('6 - Sair')
@@ -20,27 +20,27 @@ while True:
     a = int(input('Qual produto você deseja comprar? '))
 
     if a == 1:
-        d = input('Você tem dinheiro para comprar carne? Custa 50 reais: ').lower()
+        d = input('Você tem dinheiro para comprar um par de Tênis? Custa 130 reais: ').lower()
         if d in ['sim', 's', 'ss']:
-            print('Beleza, você comprou Carne')
+            print('Beleza, você comprou um par de Tênis')
         elif d in ['não', 'nao', 'n', 'nn']:
             print('Ok, vá embora')
         else:
             print('Resposta inválida')
 
     elif a == 2:
-        d = input('Você tem dinheiro para comprar açúcar? Custa 10 reais: ').lower()
+        d = input('Você tem dinheiro para comprar Microfone? Custa 100 reais: ').lower()
         if d in ['sim', 's', 'ss']:
-            print('Beleza, você comprou Açúcar')
+            print('Beleza, você comprou Microfone')
         elif d in ['não', 'nao', 'n', 'nn']:
             print('Ok, vá embora')
         else:
             print('Resposta inválida')
 
     elif a == 3:
-        d = input('Você tem dinheiro para comprar chocolate? Custa 5 reais: ').lower()
+        d = input('Você tem dinheiro para comprar Mouse? Custa 50 reais: ').lower()
         if d in ['sim', 's', 'ss']:
-            print('Beleza, você comprou Chocolate')
+            print('Beleza, você comprou Mouse')
         elif d in ['não', 'nao', 'n', 'nn']:
             print('Ok, vá embora')
         else:
@@ -54,7 +54,14 @@ while True:
             print('Ok, vá embora')
         else:
             print('Resposta inválida')
-
+    elif a == 5:
+        d = print('Você tem dinheiro para comprar um Ps5? Custa 4299: ').lower()
+        if d in ['sim', 's', 'ss']:
+            print('Beleza, você comprou um Ps5')
+        elif d in ['não', 'nao', 'n', 'nn']:  
+            print('Claro que Você não tem, olha o preço disso') 
+        else:
+                   print('Resposta inválida') 
     elif a == 6:
         print('Saindo da loja...')
         break
